7e58e13a52d0bbec672744c592fbdd1716e8a01a
hp28190 <hp28190@hotmail.fr>
2018-10-08 14:21:19 +0200

    Ajout de main.c, programme changeant la coloration de l'invite de commande windows

---
+++ README.txt
@@ -0,0 +1,6 @@
+# test-C
+test pour formation
+
+Copie du repo GitHUB et modification du readme.md
+
+
---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+*	Main.c
+*	Fichier principal du projet de test Git 
+*
+*	Auteur LOUIS Maxime
+*
+*/
+#include <windows.h>
+
+int main (void)
+{
+	system('COLOR AF');
+	
+	
+	system('PAUSE');
+	
+	return 0;
+}
